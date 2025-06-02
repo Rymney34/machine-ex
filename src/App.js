1bb69ba0d3fbbd658ac7b03fc93cc6499b8c18ef
Rymney34 <locktimil70@gmail.com>
2025-06-03 00:25:28 +0100

    finished applicaiton where you can chnage quotes by cliking button data fetehed from api

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
 
-import { useEffect, useState } from 'react';
 import './App.css';
 import './machineQ.css';
+import Qoutes from './getQuote'
 import Axios from "axios";
-
 
 
 export function Machine() {
 
-  const [data, setData] = useState('');
-
-  useEffect(() => {
-  Axios.get("http://api.quotable.io/random")
-  .then(res => setData(res.data))
-  .catch(err => console.log(err));
-  },[])
-
-  // fetch("http://api.quotable.io/random")
-  // .then((res) => res.json())
-  // .then((data) => {
-  //   console.log(data)
-  // });
-
-
-
   return (
    <div id='wrapper'>
-
-      <div id="quote-box">
-          <h1 id='text'>
-              "{data.content}
-          </h1>
-            <h3 id='author'>
-               - {data.author}
-            </h3>
-              <div id='new-quote'>
-                <a id='tweet-quote'>
-                  New quote
-                </a>
-              </div>
-      
-      </div>
+        <Qoutes/>
       <div class="credits">
         ByTim-Rymney34
       </div>
--- src/getQuote.js
+++ src/getQuote.js
@@ -1,25 +1,60 @@
+import { useEffect, useState } from 'react';
+import Axios from "axios";
+import './machineQ.css';
 
 function Qoutes () {
 
-//     const getQuote = async () => {
-//         try{
-//             const response = await fetch('https://api.quotable.io/random');
-//             const data = await response.json();
-//             setQuote(data.tags + ' — ' + data.author);
-//             console.log(data.tags, data.author)
-//         }
-//         catch (error) {
-//             console.error('Error fetching quote:', error);
-//             setQuote('Failed to fetch quote.');
-//         }
+  const [data, setData] = useState('');
 
-//     };
+  const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
 
-//     getQuote();
+  const fetchQuote = () => {
 
-// }
+     const color = getRandomColor();
+    Axios.get("http://api.quotable.io/random")
+    .then(res => setData(res.data))
+    .catch(err => console.log(err));
 
-// function App() {
- 
+      document.body.style.backgroundColor = color;
+        document.getElementById("text").style.color = color;
+        document.getElementById("new-quote").style.backgroundColor = color;
+         document.getElementById("author").style.color = color;
+         document.getElementById("tweet-quote").style.color = "white";         
+    //  document.body.style.transition = "1s ease"
+     
+  }
+
+  
+
+  useEffect(() => {
+  Axios.get("http://api.quotable.io/random")
+  .then(res => setData(res.data))
+  .catch(err => console.log(err));
+  },[])
+
+    return (
+        <div id="quote-box">
+         <h1 id='text'>
+              "{data.content}
+          </h1>
+            <h3 id='author'>
+               - {data.author}
+            </h3>
+             
+             
+            <button id='new-quote' onClick={fetchQuote}>
+                <a id='tweet-quote'>
+                    New quote
+                </a>
+            </button>
+        </div>
+    )
 }
-export default Qoutes();
+export default Qoutes;
